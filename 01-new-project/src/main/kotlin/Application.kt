567ecdf03b001df0e298eee6a5e02dea31117e6d
Lucas Bateson <87132749+Lucas8448@users.noreply.github.com>
2025-11-21 08:10:51 +0100

    Scaffold and finish step 1

---
+++ 01-new-project/src/main/kotlin/Application.kt
@@ -0,0 +1,18 @@
+package com.example
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
+
+fun main(args: Array<String>) = EngineMain.main(args)
+
+fun Application.module() {
+    install(ContentNegotiation) {
+        json()
+    }
+    configureRouting()
+}
