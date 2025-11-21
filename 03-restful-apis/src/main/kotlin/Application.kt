1e587885babb4c0c374e484728521e24f719a045
Lucas Bateson <87132749+Lucas8448@users.noreply.github.com>
2025-11-21 08:39:06 +0100

    Step 3

--- 03-restful-apis/src/main/kotlin/Application.kt
+++ 03-restful-apis/src/main/kotlin/Application.kt
@@ -7,6 +7,8 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.http.content.*
+import com.example.model.*
 
 fun main(args: Array<String>) = EngineMain.main(args)
 
@@ -16,6 +18,19 @@ fun Application.module() {
     }
 
     routing {
+        staticResources("static", "static")
+
+        get("/tasks") {
+            call.respond(
+                listOf(
+                    Task("cleaning", "Clean the house", Priority.Low),
+                    Task("gardening", "Mow the lawn", Priority.Medium),
+                    Task("shopping", "Buy the groceries", Priority.High),
+                    Task("painting", "Paint the fence", Priority.Medium)
+                )
+            )
+        }
+
         get("/") {
             call.respondText("Hello from Ktor")
         }
